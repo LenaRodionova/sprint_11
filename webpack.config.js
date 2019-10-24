5d8e1a9c456660a77be73a4d33859ca3d383326c
Lena Rodionova <lena199527@yandex.ru>
2019-10-24 14:31:40 +0300

    Настроила хеширование

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackMd5Hash = require('webpack-md5-hash');
 
 module.exports = {
     entry: {
@@ -7,7 +9,7 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js'
+        filename: '[name].[chunkhash].js'
     },
     module: {
         rules: [{
@@ -19,21 +21,23 @@ module.exports = {
             },
             {
                 test: /\.css$/,
-                use: [MiniCssExtractPlugin.loader, 'css-loader']
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
             },
             {
-                test: /\.svg$/,
-                loader: 'svg-inline-loader'
-            },
-            {
-                test: /\.(jpg|jpeg|png|woff|woff2|eot|ttfs)$/,
+                test: /\.(jpg|jpeg|png|woff|woff2|eot|ttfs|svg)$/,
                 loader: 'url-loader?limit=100000'
             }
         ]
     },
     plugins: [
         new MiniCssExtractPlugin({
-            filename: 'style.css'
-        })
+            filename: 'style.[contenthash].css'
+        }),
+        new HtmlWebpackPlugin({
+            inject: false, 
+            template: './src/index.html', // откуда брать образец для сравнения с текущим видом проекта
+            filename: 'index.html' // имя выходного файла, то есть того, что окажется в папке dist после сборки
+          }),
+        new WebpackMd5Hash()
     ]
 };
